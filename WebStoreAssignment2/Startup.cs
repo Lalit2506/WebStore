6b1266812c2325d6d1cd0e0c80d0ecc442a7c48e
Lalit Sharma <lalit255290@gmail.com>
2019-03-26 19:06:00 -0400

    Add project files.

---
+++ WebStoreAssignment2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebStoreAssignment2.Startup))]
+namespace WebStoreAssignment2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
