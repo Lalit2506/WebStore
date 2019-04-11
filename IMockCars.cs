b5d2a296420d8fee30eeff59a91093bee831c4fd
Sovatwattana <36393153+Sovatwattana@users.noreply.github.com>
2019-04-11 11:04:42 -0400

    Add files via upload

---
+++ IMockCars.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebStoreAssignment2.Models
+{
+    public interface IMockCars
+    {
+        IQueryable<Car> cars { get; }
+        Car Save(Car cars);
+        void Delete(Car cars);
+        void Dispose();
+    }
+}
